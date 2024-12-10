55cc7dc6e3e30aa31eab57b3adf5afaa4a42c90d
Hamza Sengul <138229016+Hamza-Sengul@users.noreply.github.com>
2024-12-10 20:17:43 +0300

    Update urls.py

--- qr_yoklama/account/urls.py
+++ qr_yoklama/account/urls.py
@@ -28,9 +28,8 @@ urlpatterns = [
     path('qr-code/<int:qr_code_id>/image/', views.qr_code_image, name='qr_code_image'),
     path('course/<int:course_id>/', views.student_course_details, name='student_course_details'),
     path('attendance-logs/', views.attendance_logs, name='attendance_logs'),
-    #path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-    path('password-reset/', views.CustomPasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
 ]
